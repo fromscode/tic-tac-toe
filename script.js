b9895c41b2e6bb7858d1431f33a01f0be2590f3a
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-06 06:08:01 +0000

    create gameboard, player and controller(wip)

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("test");
+
+let gameboard = (function createGameBoard() {
+    let board = [];
+    return {board};
+}) ();
+
+console.log(gameboard.board);
+
+
+function createPlayer(name) {
+    getName = () => name;
+    return {getName};
+}
+
+const user = createPlayer("test-player");
+
+console.log(user.getName());
+
+let gameController = (function() {
+    firstPlayerName =  prompt("Enter player 1 name: ");
+    secondPlayerName =  prompt("Enter player 2 name: ");
+    play = () => {console.log(firstPlayerName + " " + secondPlayerName)};
+    return {play};
+}) ();
+
+
+gameController.play();
